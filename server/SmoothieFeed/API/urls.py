e1a3a66f68a6b38540a4fd86239bed26652fd598
DeNeMiX83 <daniil53891@yandex.ru>
2021-11-08 23:55:18 +0300

    add use user_settings

--- server/SmoothieFeed/API/urls.py
+++ server/SmoothieFeed/API/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('users/create/', UserCreateView.as_view()),
     # path('users/<int:pk>/', UserDetailView.as_view()),
     # path('users/<int:pk>/check_password', UserDetailView.as_view()),
-    # path('users/<int:user_id>/settings/', UserSettingsUpdateView.as_view()),
+    path('users/settings/', UserSettingsUpdateView.as_view()),
     # path('users/<int:user_id>/subscibes/create/', SubscribesCreateView.as_view()),
     path('social_media/public/create/', SocialMediaPublicCreateView.as_view()),
 ]
--- server/SmoothieFeed/API/views.py
+++ server/SmoothieFeed/API/views.py
@@ -4,10 +4,12 @@ from rest_framework.generics import (CreateAPIView,
                                      RetrieveUpdateDestroyAPIView,
                                      UpdateAPIView,
                                      RetrieveAPIView,
-                                     RetrieveUpdateAPIView, ListCreateAPIView,
+                                     RetrieveUpdateAPIView, ListCreateAPIView, get_object_or_404,
                                      )
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication, JWTTokenUserAuthentication
 
 from .models import *
 from .serializers import *
@@ -27,15 +29,31 @@ class UserCreateView(CreateAPIView):
 #     queryset = User.objects.all()
 
 
-# class UserSettingsUpdateView(UpdateAPIView):
-#     serializer_class = UserSettingsSerializer
-#     queryset = UserSettings.objects.all()
-#     lookup_field = 'user_id'
+class UserSettingsUpdateView(UpdateAPIView, JWTTokenUserAuthentication):
+    permission_classes = [IsAuthenticated]
+    queryset = UserSettings.objects.all()
+    serializer_class = UserSettingsSerializer
+    user_id = None
+
+    def put(self, request, *args, **kwargs):
+        user,  validated_token = self.authenticate(request)
+        self.user_id = user.pk
+        return self.update(request, *args, **kwargs)
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        filter_kwargs = {'user_id': self.user_id}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
 
 class SocialMediaPublicCreateView(CreateAPIView):
     serializer_class = SocialMediaPublicSerializer
     queryset = SocialMediaPublic.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         serializer = SocialMediaPublicSerializer(data=request.data, many=True)
