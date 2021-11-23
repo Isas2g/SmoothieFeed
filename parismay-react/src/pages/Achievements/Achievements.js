778d00d626593ca58d40ff51b950b6469aecbacc
Denis Kamchatov <kamcatovdenis@gmail.com>
2021-11-23 18:33:27 +0500

    Adaptability to the mobile version of the achievements page

--- parismay-react/src/pages/Achievements/Achievements.js
+++ parismay-react/src/pages/Achievements/Achievements.js
@@ -4,7 +4,7 @@ import AchievementsList from "@components/AchievementsList/AchievementsList";
 import "./Achievements.scss";
 
 const Achievements = () => {
-	return <main className="main">
+	return <main className="main main__achievements">
 		<div className="container">
 			<div className="main__achievements achievements">
 				<h1 className="achievements__title main-title">Достижения</h1>
