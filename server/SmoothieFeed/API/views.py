c0edc4de077abcbf207fd5030338c02839c5b5b5
DeNeMiX83 <daniil53891@yandex.ru>
2021-12-18 14:34:23 +0300

    add get user info

--- server/SmoothieFeed/API/views.py
+++ server/SmoothieFeed/API/views.py
@@ -3,7 +3,6 @@ from rest_framework.generics import ListAPIView
 from .serializers import *
 
 
-@swagger_auto_schema()
 class DetoxLevelView(ListAPIView):
     serializer_class = DetoxLevelSerializer
     queryset = DetoxLevel.objects.all()
